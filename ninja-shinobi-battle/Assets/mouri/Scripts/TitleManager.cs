99635b85d004d5db9be1d9278e1b881901314d3f
NobuyukiMouri <hoge@hoge.com>
2015-10-24 15:11:40 +0900

    try to commit..

---
+++ ninja-shinobi-battle/Assets/mouri/Scripts/TitleManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TitleManager : MonoBehaviour {
+
+    GameObject Fade;    //フェードに使うGameObject
+    bool FadeInFlg;     //フェードインに使うフラグ
+    bool FadeOutFlg;    //フェードアウトに使うフラグ
+    float StoryTime;
+    GameObject gStory;
+    GameObject StartButton;
+    GameObject TitleRogo;
+    //初期化
+	void Start () {
+        StartButton = GameObject.Find("StartButton");
+        StartButton.SetActive(false);
+        TitleRogo = GameObject.Find("TitleRogo");
+        TitleRogo.SetActive(false);
+        Fade = GameObject.Find("Fade");
+        gStory = GameObject.Find("Story");
+        FadeInFlg = true;
+        FadeOutFlg = false;
+    }
+	//アップデート
+	void Update () {
+        Story();
+        FadeIn();
+        if (FadeOutFlg){
+            SceneChange();
+        }
+        
+    }
+    //FadeIn
+    void FadeIn(){
+        //フェードインさせる
+        if (FadeInFlg){
+            Fade.GetComponent<Image>().color += new Color(0.0f, 0.0f, 0.0f, -0.001f);
+        }
+        //alphaが0になったら止める
+            if (Fade.GetComponent<Image>().color.a <= 0.1f){
+                    FadeInFlg = false;       
+            }
+    }    
+    //ステージ切り替え
+    public void SceneChange(){
+        FadeOutFlg = true;
+        //フェードアウトしてシーンを切り替える
+        Fade.GetComponent<Image>().color += new Color(0.0f, 0.0f, 0.0f, 0.01f);
+            if (Fade.GetComponent<Image>().color.a >= 0.9f){
+                Application.LoadLevel("Main");
+            }
+    }
+    //ストーリー関連
+    void Story(){
+        StoryTime += Time.deltaTime;
+        if (StoryTime >= 12.0f){
+            gStory.SetActive(false);
+            StartButton.SetActive(true);
+            TitleRogo.SetActive(true);
+        }
+    }
+}
+
