563fe8282eec6ad9edc8b6795ed68100f0343109
NobuyukiMouri <hoge@hoge.com>
2015-10-24 15:41:55 +0900

    OPとED

--- ninja-shinobi-battle/Assets/mouri/Scripts/Ending.cs
+++ ninja-shinobi-battle/Assets/mouri/Scripts/Ending.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class TitleManager : MonoBehaviour {
+public class Ending : MonoBehaviour {
 
     GameObject Fade;    //フェードに使うGameObject
     bool FadeInFlg;     //フェードインに使うフラグ
@@ -9,54 +9,64 @@ public class TitleManager : MonoBehaviour {
     float StoryTime;
     GameObject gStory;
     GameObject StartButton;
-    GameObject TitleRogo;
+    //GameObject TitleRogo;
     //初期化
-	void Start () {
-        StartButton = GameObject.Find("StartButton");
+    void Start()
+    {
+        StartButton = GameObject.Find("ModoruButton");
         StartButton.SetActive(false);
-        TitleRogo = GameObject.Find("TitleRogo");
-        TitleRogo.SetActive(false);
+     // TitleRogo = GameObject.Find("TitleRogo");
+     // TitleRogo.SetActive(false);
         Fade = GameObject.Find("Fade");
         gStory = GameObject.Find("Story");
         FadeInFlg = true;
         FadeOutFlg = false;
     }
-	//アップデート
-	void Update () {
+    //アップデート
+    void Update()
+    {
         Story();
         FadeIn();
-        if (FadeOutFlg){
+        if (FadeOutFlg)
+        {
             SceneChange();
         }
-        
+
     }
     //FadeIn
-    void FadeIn(){
+    void FadeIn()
+    {
         //フェードインさせる
-        if (FadeInFlg){
+        if (FadeInFlg)
+        {
             Fade.GetComponent<Image>().color += new Color(0.0f, 0.0f, 0.0f, -0.001f);
         }
         //alphaが0になったら止める
-            if (Fade.GetComponent<Image>().color.a <= 0.1f){
-                    FadeInFlg = false;       
-            }
-    }    
+        if (Fade.GetComponent<Image>().color.a <= 0.1f)
+        {
+            FadeInFlg = false;
+        }
+    }
     //ステージ切り替え
-    public void SceneChange(){
+    public void SceneChange()
+    {
         FadeOutFlg = true;
         //フェードアウトしてシーンを切り替える
         Fade.GetComponent<Image>().color += new Color(0.0f, 0.0f, 0.0f, 0.01f);
-            if (Fade.GetComponent<Image>().color.a >= 0.9f){
-                Application.LoadLevel("MainScene");
-            }
+        if (Fade.GetComponent<Image>().color.a >= 0.9f)
+        {
+            Application.LoadLevel("OpeningScene");
+        }
     }
     //ストーリー関連
-    void Story(){
+    void Story()
+    {
         StoryTime += Time.deltaTime;
-        if (StoryTime >= 12.0f){
+        if (StoryTime >= 12.0f)
+        {
             gStory.SetActive(false);
             StartButton.SetActive(true);
-            TitleRogo.SetActive(true);
+            //TitleRogo.SetActive(true);
         }
     }
 }
