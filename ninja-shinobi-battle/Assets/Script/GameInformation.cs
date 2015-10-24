083f21784072dd968bf0444b62bf8585f928f5d2
sakuriver <suzue@yahoo.co.jp>
2015-10-24 14:32:16 +0900

    ディレクトリの整理と敵の生成部分を仮で作った

--- ninja-shinobi-battle/Assets/Script/GameInformation.cs
+++ ninja-shinobi-battle/Assets/Script/GameInformation.cs
@@ -7,4 +7,21 @@ public class GameInformation : MonoBehaviour {
 	public static int WEAPON_CODE_BOMB = 1;
 
 	public int selectWeapon = 0;
+	public int enemyCreateCount = 0;
+	void Start() {
+	}
+
+	public void Update() {
+		if (enemyCreateCount > 70) {
+			// プレハブを取得
+			GameObject prefab = (GameObject)Resources.Load ("Prefab/Enemy");
+			Vector3 position = new Vector3 ((Random.value * 8) - (Random.value * 8), (Random.value * 4) - (Random.value * 4), -10);
+			// プレハブからインスタンスを生成
+			Instantiate (prefab, position, Quaternion.identity);
+			Debug.Log ("enemy create complete");
+			enemyCreateCount=0;
+		}
+		enemyCreateCount++;
+	}
+
 }
